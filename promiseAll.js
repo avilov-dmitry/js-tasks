b3571c2dc249b5d7bc1e4acbb9bc5f9df92ecf9d
Avilov Dmitry <dmitry.avilov@raiffeisen.ru>
2020-02-27 10:51:08 +0300

    init + promiseAll

---
+++ promiseAll.js
@@ -0,0 +1,21 @@
+const a = new Promise((resolve) => setTimeout(() => resolve(1),3000));
+const b = new Promise((resolve) => setTimeout(() => resolve(2),2000));
+const c = new Promise((resolve) => setTimeout(() => resolve(3),1000));
+
+const promiseAll = (arr) => {
+    const result = [];
+
+    return new Promise(resolve => {
+        return arr.forEach(promise => {
+            promise.then(item => {
+                result.push(item);
+                if(arr.length === result.length) {
+                    resolve(result);
+                }
+            })
+        });
+    })
+};
+
+promiseAll([a, b, c]).then(result => console.log(result));
+
